d936523dc479b220ade4f06903358ecec3ce3aa3
Griffin <35904717+gjbsaiia@users.noreply.github.com>
2018-08-10 10:38:33 +0000

    Add files via upload

--- moveToDirectory.sh
+++ moveToDirectory.sh
@@ -1,2 +1,2 @@
-sudo cp ~/htmlDisplay/index.php /var/www/html/
-sudo cp ~/htmlDisplay/calculateRTime.py /var/www/html/
+sudo cp ~/Desktop/htmlDisplay/index.php /var/www/html/
+sudo cp ~/Desktop/htmlDisplay/calculateRTime.py /var/www/html/
